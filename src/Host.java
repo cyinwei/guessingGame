b4fc7c20365ee61a483f77b3d826983a59c60491
pjdeavers <pjdeavers@yahoo.com>
2015-12-03 18:27:43 -0600

    Update Host.java

--- src/Host.java
+++ src/Host.java
@@ -1,3 +1,5 @@
+import java.net.Socket;
+
 /**
  * The host is the source of the game.  The host chooses a word, a category, and a hint.  The word is the secret, and
  *  the category and hint will be public.  The name of the game is for all the players (all other connections) to guess
@@ -7,4 +9,8 @@ public class Host extends Connection {
     private String secret; //current secret word
     private String category; //another "hint" for the players
     private String hint; //description given to other players on what to guess, can be changed
+
+    public Host(Socket socket, String serverMessage) {
+        super(socket, serverMessage);
+    }
 }
