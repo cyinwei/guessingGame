6a793e1add5a42b9a6c9dc8f15fbe20aa044c4fd
pjdeavers <pjdeavers@yahoo.com>
2015-12-04 09:24:57 -0600

    Update Host.java

--- src/Host.java
+++ src/Host.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.net.Socket;
+import java.util.HashSet;
 
 /**
  * The host is the source of the game.  The host chooses a word, a category, and a hint.  The word is the secret, and
@@ -6,13 +8,52 @@ import java.net.Socket;
  *  the word.
  */
 public class Host extends Connection {
+    private Socket socket;
     private String secret; //current secret word
     private String category; //another "hint" for the players
     private String hint; //description given to other players on what to guess, can be changed
     public boolean gameIsStarted; //to transition from lobby to game
+    private PrintWriter out;
+    private boolean command;
+    private BufferedReader bReader;
+    private String serverMessage; //default server message
+    private static HashSet<PrintWriter> writers = new HashSet<>();
 
     public Host(Socket socket, String serverMessage) {
         super(socket, serverMessage);
+        this.socket = socket;
+        this.serverMessage = serverMessage;
+        try {
+            //initialize our writers and readers
+            //this.writer = new PrintWriter(this.socket.getOutputStream());
+            InputStreamReader isReader = new InputStreamReader(this.socket.getInputStream());
+            this.bReader = new BufferedReader(isReader);
+            this.out = new PrintWriter(socket.getOutputStream(), true);
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void run(){
+        if (socket == null) {
+            System.out.println("Error: in SocketManager, input socket is null.");
+            return;
+        }
+        try {
+            if(bReader.readLine().equals("\\startgame")){
+                gameIsStarted=true;
+                for (PrintWriter writer: writers){
+                    writer.println("Game is starting");
+                }
+                System.out.println("Game started by" + getName());
+                command=true;
+            }
+
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     //setting up the functions unique to Host
