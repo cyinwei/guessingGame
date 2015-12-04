1908426ab97be19e03e5bf134985fbe53e0bb244
pjdeavers <pjdeavers@yahoo.com>
2015-12-04 01:36:44 -0600

    Update Host.java
    goin to bed

--- src/Host.java
+++ src/Host.java
@@ -9,8 +9,17 @@ public class Host extends Connection {
     private String secret; //current secret word
     private String category; //another "hint" for the players
     private String hint; //description given to other players on what to guess, can be changed
+    public boolean gameIsStarted; //to transition from lobby to game
 
     public Host(Socket socket, String serverMessage) {
         super(socket, serverMessage);
     }
+
+    //setting up the functions unique to Host
+    public String getSecret() { return this.secret; } //this is used to return the word that other players are guessing for within our code
+    private void setSecret(String secret) { this.secret = secret; } //this function is called when we write the \setsecret command
+    public String getCategory() { return this.category; } //this is used to return the category that the Host sets for the secret
+    private void setCategory(String category) { this.category = category; } //this function is called when we write the \setcategory command
+    public String getHint() { return this.hint; } //this is used to return the hint that the Host sets for the secret
+    private void setHint(String hint) { this.hint = hint; } //this function is called when we write the \sethint command
 }
